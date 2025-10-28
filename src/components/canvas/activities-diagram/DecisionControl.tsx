ae2ae6a222f022deb9d4ad4c3ba68d992f80ee70
DaniVegaM <dany.17.com@hotmail.com>
2025-10-28 12:41:52 -0600

    feat: creating decision node without edges

---
+++ src/components/canvas/activities-diagram/DecisionControl.tsx
@@ -0,0 +1,43 @@
+import { Position, useNodeId } from "@xyflow/react";
+import BaseHandle from "../BaseHandle";
+import { useNode } from "../useNode";
+import { useCanvas } from "../../../hooks/useCanvas";
+
+export default function DecitionControl() {
+    const { showSourceHandleOptions, setShowSourceHandleOptions, showTargetHandleOptions, setShowTargetHandleOptions } = useNode();
+    const { isTryingToConnect } = useCanvas();
+    const nodeId = useNodeId();
+
+    const onMouseEnter = () => {
+        if (isTryingToConnect.isTrying && isTryingToConnect.sourceNodeId !== nodeId) {
+            setShowSourceHandleOptions(false);
+            setShowTargetHandleOptions(true);
+        } else {
+            setShowTargetHandleOptions(false);
+            setShowSourceHandleOptions(true);
+        }
+    }
+
+    const onMouseLeave = () => {
+        setShowSourceHandleOptions(false);
+        setShowTargetHandleOptions(false);
+    }
+
+    return (
+        <div
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+            className="p-2 bg-gray-300 dark:bg-neutral-800 hover:bg-gray-200 dark:hover:bg-zinc-600 min-w-[50px] min-h-[50px] flex flex-col items-center justify-center transition-all duration-150"
+            style={{
+                clipPath: 'polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)',
+            }}
+        >
+            <BaseHandle id={0} type="target" position={Position.Left} maxTargetConnections={1} maxSourceConnections={3} showSourceHandleOptions={showSourceHandleOptions} showTargetHandleOptions={showTargetHandleOptions} className="!w-3 !h-3" />
+            <BaseHandle id={1} type="target" position={Position.Top} maxTargetConnections={1} maxSourceConnections={3} showSourceHandleOptions={showSourceHandleOptions} showTargetHandleOptions={showTargetHandleOptions} className="!w-3 !h-3" />
+            <BaseHandle id={2} type="source" position={Position.Top} maxTargetConnections={1} maxSourceConnections={3} showSourceHandleOptions={showSourceHandleOptions} showTargetHandleOptions={showTargetHandleOptions} className="!w-3 !h-3" />
+            <BaseHandle id={3} type="source" position={Position.Right} maxTargetConnections={1} maxSourceConnections={3} showSourceHandleOptions={showSourceHandleOptions} showTargetHandleOptions={showTargetHandleOptions} className="!w-3 !h-3" />
+            <BaseHandle id={4} type="source" position={Position.Bottom} maxTargetConnections={1} maxSourceConnections={3} showSourceHandleOptions={showSourceHandleOptions} showTargetHandleOptions={showTargetHandleOptions} className="!w-3 !h-3" />
+            <BaseHandle id={5} type="source" position={Position.Left} maxTargetConnections={1} maxSourceConnections={3} showSourceHandleOptions={showSourceHandleOptions} showTargetHandleOptions={showTargetHandleOptions} className="!w-3 !h-3" />
+        </div >
+    )
+}
