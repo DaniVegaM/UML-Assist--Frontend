399299bc3941475e34767f77cb723ceed2548b18
0ambar <abrahamr677@gmail.com>
2026-03-16 21:12:37 -0600

    refactor: changing ordering buttons positions and decreasing their size

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -269,59 +269,60 @@ export default function Dashboard() {
                             Nuevo diagrama
                         </button>
                     </div>
-                <div className="flex gap-3">
-                    <button
-                        onClick={() => {
-                            if (ordering === "title") setOrdering("-title")
-                            else setOrdering("title")
-                        }}
-                        className={`flex items-center gap-2 px-4 py-3 rounded-lg transition-all duration-200
-                        ${
-                        ordering === "title" || ordering === "-title"
-                        ? "bg-sky-600 text-white"
-                        : "bg-zinc-200 dark:bg-zinc-700 text-zinc-700 dark:text-zinc-300 hover:bg-zinc-300 dark:hover:bg-zinc-600"
-                        }`}
-                        >
+                    <div className="flex gap-3">
+                        
+                        <button
+                            onClick={() => {
+                                if (ordering === "-created_at") setOrdering("created_at")
+                                else setOrdering("-created_at")
+                            }}
+                            className={`flex items-center gap-2 px-4 py-3 rounded-lg transition-all duration-200
+                            ${
+                            ordering === "-created_at" || ordering === "created_at"
+                            ? "bg-sky-600 text-white"
+                            : "bg-zinc-200 dark:bg-zinc-700 text-zinc-700 dark:text-zinc-300 hover:bg-zinc-300 dark:hover:bg-zinc-600"
+                            }`}
+                            >
 
-                        {ordering === "-title" ? (
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M10.082 5.629 9.664 7H8.598l1.789-5.332h1.234L13.402 7h-1.12l-.419-1.371zm1.57-.785L11 2.687h-.047l-.652 2.157z"/>
-                            <path d="M12.96 14H9.028v-.691l2.579-3.72v-.054H9.098v-.867h3.785v.691l-2.567 3.72v.054h2.645zm-8.46-.5a.5.5 0 0 1-1 0V3.707L2.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.5.5 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L4.5 3.707z"/>
-                        </svg>
-                        ) : (
+                            {ordering === "created_at" ? (
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"  viewBox="0 0 16 16">
+                                <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
+                                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
+                            </svg>
+                            ) : (
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
+                            </svg>
+                            )}
+                        </button>
 
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"  viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M10.082 5.629 9.664 7H8.598l1.789-5.332h1.234L13.402 7h-1.12l-.419-1.371zm1.57-.785L11 2.687h-.047l-.652 2.157z"/>
-                            <path d="M12.96 14H9.028v-.691l2.579-3.72v-.054H9.098v-.867h3.785v.691l-2.567 3.72v.054h2.645zM4.5 2.5a.5.5 0 0 0-1 0v9.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L4.5 12.293z"/>
-                        </svg>
-                        )}
-                    </button>
+                        <button
+                            onClick={() => {
+                                if (ordering === "title") setOrdering("-title")
+                                else setOrdering("title")
+                            }}
+                            className={`flex items-center gap-2 px-4 py-3 rounded-lg transition-all duration-200
+                            ${
+                            ordering === "title" || ordering === "-title"
+                            ? "bg-sky-600 text-white"
+                            : "bg-zinc-200 dark:bg-zinc-700 text-zinc-700 dark:text-zinc-300 hover:bg-zinc-300 dark:hover:bg-zinc-600"
+                            }`}
+                            >
 
-                    <button
-                        onClick={() => {
-                            if (ordering === "-created_at") setOrdering("created_at")
-                            else setOrdering("-created_at")
-                        }}
-                        className={`flex items-center gap-2 px-4 py-3 rounded-lg transition-all duration-200
-                        ${
-                        ordering === "-created_at" || ordering === "created_at"
-                        ? "bg-sky-600 text-white"
-                        : "bg-zinc-200 dark:bg-zinc-700 text-zinc-700 dark:text-zinc-300 hover:bg-zinc-300 dark:hover:bg-zinc-600"
-                        }`}
-                        >
+                            {ordering === "-title" ? (
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M10.082 5.629 9.664 7H8.598l1.789-5.332h1.234L13.402 7h-1.12l-.419-1.371zm1.57-.785L11 2.687h-.047l-.652 2.157z"/>
+                                <path d="M12.96 14H9.028v-.691l2.579-3.72v-.054H9.098v-.867h3.785v.691l-2.567 3.72v.054h2.645zm-8.46-.5a.5.5 0 0 1-1 0V3.707L2.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.5.5 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L4.5 3.707z"/>
+                            </svg>
+                            ) : (
 
-                        {ordering === "created_at" ? (
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"  viewBox="0 0 16 16">
-                            <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
-                            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
-                        </svg>
-                        ) : (
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
-                        </svg>
-                        )}
-                    </button>
-                </div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"  viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M10.082 5.629 9.664 7H8.598l1.789-5.332h1.234L13.402 7h-1.12l-.419-1.371zm1.57-.785L11 2.687h-.047l-.652 2.157z"/>
+                                <path d="M12.96 14H9.028v-.691l2.579-3.72v-.054H9.098v-.867h3.785v.691l-2.567 3.72v.054h2.645zM4.5 2.5a.5.5 0 0 0-1 0v9.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L4.5 12.293z"/>
+                            </svg>
+                            )}
+                        </button>
+                    </div>
                 </div>
 
                         <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 p-10">
